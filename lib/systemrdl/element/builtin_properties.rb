07f5c3d11342b44736bc96ba6e9683e6a7594361
Taichi Ishitani <taichi730@gmail.com>
2023-11-27 00:18:32 +0900

    implement field properties

--- lib/systemrdl/element/builtin_properties.rb
+++ lib/systemrdl/element/builtin_properties.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ModuleLength
+
 module SystemRDL
   module Element
     #
@@ -11,6 +13,7 @@ module SystemRDL
       ]
       property.type :string
       property.dynamic_assign true
+      property.value ''
     end
 
     RootInstance.define_builtin_property(:desc) do |property|
@@ -19,18 +22,21 @@ module SystemRDL
       ]
       property.type :string
       property.dynamic_assign true
+      property.value ''
     end
 
     RootInstance.define_builtin_property(:donttest) do |property|
       property.target :field
       property.type [:boolean, :bit]
       property.dynamic_assign true
+      property.value false
     end
 
     RootInstance.define_builtin_property(:dontcompare) do |property|
       property.target :field
       property.type [:boolean, :bit]
       property.dynamic_assign true
+      property.value false
     end
 
     #
@@ -44,5 +50,226 @@ module SystemRDL
       property.dynamic_assign true
       property.value true
     end
+
+    #
+    # 9.4 Field access properties
+    #
+    RootInstance.define_builtin_property(:hw) do |property|
+      property.target :field
+      property.type :accesstype
+      property.dynamic_assign false
+      property.value :rw
+    end
+
+    RootInstance.define_builtin_property(:sw) do |property|
+      property.target :field
+      property.type :accesstype
+      property.dynamic_assign true
+      property.value :rw
+    end
+
+    #
+    # 9.5 Hardware signal properties
+    #
+    RootInstance.define_builtin_property(:next) do |property|
+      property.target :field
+      property.type :reference
+      property.dynamic_assign true
+      property.ref_target true
+    end
+
+    RootInstance.define_builtin_property(:reset) do |property|
+      property.target :field
+      property.type [:bit, :reference]
+      property.dynamic_assign true
+      property.ref_target true
+    end
+
+    RootInstance.define_builtin_property(:resetsignal) do |property|
+      property.target :field
+      property.type :reference
+      property.dynamic_assign true
+      property.ref_target true
+    end
+
+    #
+    # 9.6 Software access properties
+    #
+    RootInstance.define_builtin_property(:rclr) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:rset) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:onread) do |property|
+      property.target :field
+      property.type :onreadtype
+      property.dynamic_assign true
+    end
+
+    RootInstance.define_builtin_property(:woset) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:woclr) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:onwrite) do |property|
+      property.target :field
+      property.type :onwritetype
+      property.dynamic_assign true
+    end
+
+    RootInstance.define_builtin_property(:swwe) do |property|
+      property.target :field
+      property.type [:boolean, :reference]
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:swwel) do |property|
+      property.target :field
+      property.type [:boolean, :reference]
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:swmod) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:swacc) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:singlepulse) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.value false
+    end
+
+    #
+    # 9.7 Hardware access properties
+    #
+    RootInstance.define_builtin_property(:we) do |property|
+      property.target :field
+      property.type [:boolean, :reference]
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:wel) do |property|
+      property.target :field
+      property.type [:boolean, :reference]
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:anded) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:ored) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:xored) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:fieldwidth) do |property|
+      property.target :field
+      property.type :longint
+      property.dynamic_assign false
+    end
+
+    RootInstance.define_builtin_property(:hwclr) do |property|
+      property.target :field
+      property.type [:boolean, :reference]
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:hwset) do |property|
+      property.target :field
+      property.type [:boolean, :reference]
+      property.dynamic_assign true
+      property.ref_target true
+      property.value false
+    end
+
+    RootInstance.define_builtin_property(:hwenable) do |property|
+      property.target :field
+      property.type :reference
+      property.dynamic_assign true
+      property.ref_target true
+    end
+
+    RootInstance.define_builtin_property(:hwmask) do |property|
+      property.target :field
+      property.type :reference
+      property.dynamic_assign true
+      property.ref_target true
+    end
+
+    #
+    # 9.10 Miscellaneous field properties
+    #
+    RootInstance.define_builtin_property(:precedence) do |property|
+      property.target :field
+      property.type :precedencetype
+      property.dynamic_assign true
+      property.value :sw
+    end
+
+    RootInstance.define_builtin_property(:paritycheck) do |property|
+      property.target :field
+      property.type :boolean
+      property.dynamic_assign false
+      property.value false
+    end
   end
 end
+
+# rubocop:enable Metrics/ModuleLength
--- spec/systemrdl/element/field_instance_spec.rb
+++ spec/systemrdl/element/field_instance_spec.rb
@@ -11,14 +11,54 @@ RSpec.describe SystemRDL::Element::FieldInstance do
 
   describe 'properties' do
     it 'should have general component properties' do
-      expect(field).to have_property(:name, type: :string, dynamic_assign: true)
-      expect(field).to have_property(:desc, type: :string, dynamic_assign: true)
-      expect(field).to have_property(:donttest, type: [:boolean, :bit], dynamic_assign: true)
-      expect(field).to have_property(:dontcompare, type: [:boolean, :bit], dynamic_assign: true)
+      expect(field).to have_property(:name, type: :string, dynamic_assign: true, value: '')
+      expect(field).to have_property(:desc, type: :string, dynamic_assign: true, value: '')
+      expect(field).to have_property(:donttest, type: [:boolean, :bit], dynamic_assign: true, value: false)
+      expect(field).to have_property(:dontcompare, type: [:boolean, :bit], dynamic_assign: true, value: false)
     end
 
     it 'should have the content deprecation property' do
       expect(field).to have_property(:ispresent, type: :boolean, dynamic_assign: true, value: true)
+    end
+
+    it 'should have field specific properties' do
+      # 9.4 Field access properties
+      expect(field).to have_property(:hw, type: :accesstype, dynamic_assign: false, value: :rw)
+      expect(field).to have_property(:sw, type: :accesstype, dynamic_assign: true, value: :rw)
+
+      # 9.5 Hardware signal properties
+      expect(field).to have_property(:next, type: :reference, dynamic_assign: true, ref_target: true)
+      expect(field).to have_property(:reset, type: [:bit, :reference], dynamic_assign: true, ref_target: true)
+      expect(field).to have_property(:resetsignal, type: :reference, dynamic_assign: true, ref_target: true)
+
+      # 9.6 Software access properties
+      expect(field).to have_property(:rclr, type: :boolean, dynamic_assign: true, value: false)
+      expect(field).to have_property(:rset, type: :boolean, dynamic_assign: true, value: false)
+      expect(field).to have_property(:onread, type: :onreadtype, dynamic_assign: true)
+      expect(field).to have_property(:woset, type: :boolean, dynamic_assign: true, value: false)
+      expect(field).to have_property(:woclr, type: :boolean, dynamic_assign: true, value: false)
+      expect(field).to have_property(:onwrite, type: :onwritetype, dynamic_assign: true)
+      expect(field).to have_property(:swwe, type: [:boolean, :reference], dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:swwel, type: [:boolean, :reference], dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:swmod, type: :boolean, dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:swacc, type: :boolean, dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:singlepulse, type: :boolean, dynamic_assign: true, value: false)
+
+      # 9.7 Hardware access properties
+      expect(field).to have_property(:we, type: [:boolean, :reference], dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:wel, type: [:boolean, :reference], dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:anded, type: :boolean, dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:ored, type: :boolean, dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:xored, type: :boolean, dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:fieldwidth, type: :longint, dynamic_assign: false)
+      expect(field).to have_property(:hwclr, type: [:boolean, :reference], dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:hwset, type: [:boolean, :reference], dynamic_assign: true, ref_target: true, value: false)
+      expect(field).to have_property(:hwenable, type: :reference, dynamic_assign: true, ref_target: true)
+      expect(field).to have_property(:hwmask, type: :reference, dynamic_assign: true, ref_target: true)
+
+      # 9.10 Miscellaneous field properties
+      expect(field).to have_property(:precedence, type: :precedencetype, dynamic_assign: true, value: :sw)
+      expect(field).to have_property(:paritycheck, type: :boolean, dynamic_assign: false, value: false)
     end
   end
 end
