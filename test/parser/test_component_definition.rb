40e53f38852369879426760faa46576a9e8d691f
Taichi Ishitani <taichi730@gmail.com>
2026-02-08 23:48:28 +0900

    add tests for `mem` and `addrmap` component definitions

--- test/parser/test_component_definition.rb
+++ test/parser/test_component_definition.rb
@@ -209,7 +209,7 @@ module SystemRDL
               component_insts(component_inst(:f1))
             ),
             post_prop_assignment(
-              :f1, :name, string('"New name for Field 1"')
+              [:f1], :name, string('"New name for Field 1"')
             ),
             component_insts(
               component_inst(:some_reg)
@@ -240,6 +240,60 @@ module SystemRDL
               component_insts(
                 component_inst(:b, array(32), reset_value(number(1)))
               )
+            )
+          ),
+          code
+        )
+      end
+
+      def test_memory_component
+        code = <<~'M'
+          mem fifo_mem {
+            mementries = 1024;
+            memwidth = 32;
+          };
+        M
+        assert_parses(
+          mem_named_definition(
+            id(:fifo_mem),
+            prop_assignment(:mementries, number(1024)),
+            prop_assignment(:memwidth, number(32))
+          ),
+          code
+        )
+
+        code = <<~'M'
+          external mem fifo_mem {
+            mementries = 1024;
+            memwidth = 32;
+          } mem_a, mem_b;
+        M
+        assert_parses(
+          mem_named_definition(
+            id(:fifo_mem),
+            prop_assignment(:mementries, number(1024)),
+            prop_assignment(:memwidth, number(32)),
+            external_component_insts(
+              component_inst(:mem_a),
+              component_inst(:mem_b),
+            )
+          ),
+          code
+        )
+
+        code = <<~'M'
+          mem {
+            mementries = 1024;
+            memwidth = 32;
+          } external mem_a, mem_b;
+        M
+        assert_parses(
+          mem_anonymous_definition(
+            prop_assignment(:mementries, number(1024)),
+            prop_assignment(:memwidth, number(32)),
+            external_component_insts(
+              component_inst(:mem_a),
+              component_inst(:mem_b),
             )
           ),
           code
@@ -358,12 +412,138 @@ module SystemRDL
         )
       end
 
+      def test_address_map
+        code = <<~'AM'
+          addrmap some_bridge {
+            desc="overlapping address maps with both shared register space and orthogonal register space";
+            bridge;
+            reg status {
+              shared;
+              field {
+                hw=rw;
+                sw=r;
+              } stat1 = 1'b0;
+            };
+            reg some_axi_reg {
+              field {
+                desc="credits on the AXI interface";
+              } credits[4] = 4'h7;
+            };
+            reg some_ahb_reg {
+              field {
+                desc="credits on the AHB Interface";
+              } credits[8] = 8'b00000011;
+            };
+
+            addrmap {
+              littleendian;
+              some_ahb_reg ahb_credits;
+              status ahb_stat @0x20;
+              ahb_stat.stat1->desc = "bar";
+            } ahb;
+          };
+        AM
+        assert_parses(
+          addrmap_named_definition(
+            id(:some_bridge),
+            prop_assignment(
+              :desc,
+              string('"overlapping address maps with both shared register space and orthogonal register space"')
+            ),
+            prop_assignment(:bridge),
+            reg_named_definition(
+              id(:status),
+              prop_assignment(:shared),
+              field_anonymous_definition(
+                prop_assignment(:hw, access_type(:rw)),
+                prop_assignment(:sw, access_type(:r)),
+                component_insts(
+                  component_inst(
+                    :stat1,
+                    reset_value(verilog_number("1'b0"))
+                  )
+                )
+              )
+            ),
+            reg_named_definition(
+              id(:some_axi_reg),
+              field_anonymous_definition(
+                prop_assignment(
+                  :desc,
+                  string('"credits on the AXI interface"')
+                ),
+                component_insts(
+                  component_inst(
+                    :credits,
+                    array(4),
+                    reset_value(verilog_number("4'h7"))
+                  )
+                )
+              )
+            ),
+            reg_named_definition(
+              id(:some_ahb_reg),
+              field_anonymous_definition(
+                prop_assignment(
+                  :desc,
+                  string('"credits on the AHB Interface"')
+                ),
+                component_insts(
+                  component_inst(
+                    :credits,
+                    array(8),
+                    reset_value(verilog_number("8'b00000011"))
+                  )
+                )
+              )
+            ),
+            addrmap_anonymous_definition(
+              prop_assignment(:littleendian),
+              explicit_component_inst(
+                :some_ahb_reg,
+                component_insts(
+                  component_inst(:ahb_credits)
+                )
+              ),
+              explicit_component_inst(
+                :status,
+                component_insts(
+                  component_inst(
+                    :ahb_stat,
+                    address_assignment(number('0x20'))
+                  )
+                )
+              ),
+              post_prop_assignment([:ahb_stat, :stat1], :desc, string('"bar"')),
+              component_insts(component_inst(:ahb))
+            )
+          ),
+          code
+        )
+      end
+
+      def addrmap_anonymous_definition(*children)
+        s(:component_anon_def, 'addrmap', *children)
+      end
+
+      def addrmap_named_definition(*children)
+        s(:component_named_def, 'addrmap', *children)
+      end
+
       def regfile_anonymous_definition(*children)
         s(:component_anon_def, 'regfile', *children)
       end
 
       def regfile_named_definition(*children)
         s(:component_named_def, 'regfile', *children)
+      end
+
+      def mem_anonymous_definition(*children)
+        s(:component_anon_def, 'mem', *children)
+      end
+
+      def mem_named_definition(*children)
+        s(:component_named_def, 'mem', *children)
       end
 
       def reg_anonymous_definition(*children)
@@ -450,10 +630,12 @@ module SystemRDL
         s(:prop_assignment, *[id(prop_name), value].compact)
       end
 
-      def post_prop_assignment(inst_name, prop_name, value)
+      def post_prop_assignment(inst_names, prop_name, value)
+        inst_elements =
+          inst_names.map { |name| s(:instance_ref_element, id(name)) }
         prop_ref = s(
           :prop_ref,
-          s(:instance_ref, s(:instance_ref_element, id(inst_name))),
+          s(:instance_ref, *inst_elements),
           id(prop_name)
         )
         s(:post_prop_assignment, prop_ref, value)
